19b72e38b00bdb7514694a3a787a27ce429fc58e
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-18 16:44:34 +0530

    css-commit

--- firstapp/index.js
+++ firstapp/index.js
@@ -1,6 +1,14 @@
- const express =  require("express")
+ 
+const express =  require("express")
  const app = express()
+ const path = require("path")
+//allows resources in this path to be directly accesible
+app.use(express.static(path.join(__dirname, "public/scripts")))
+app.use(express.static(path.join(__dirname, "public/styles")))
 
+app.get("/main",function(request,response){
+    response.sendFile(path.join(__dirname, "public/pages/home.html"))
+})
 app.get("/",function(request,response){
     response.send("NODE WEB APP IS RUNNING")
 })
